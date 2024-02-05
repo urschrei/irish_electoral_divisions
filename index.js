7c4dd477af414d32b8d4f02fa64357fed97377ec
Stephan Hügel <shugel@tcd.ie>
2024-02-05 17:36:36 +0000

    LEA by default

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const bounds = [
 const formatter = new Intl.NumberFormat('en-IE', {
     style: 'currency',
     currency: 'EUR',
-
-    // These options are needed to round to whole numbers if that's what you want.
     minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
     maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
@@ -67,7 +65,7 @@ function getProperties(e) {
                       </div>
                 </div>
             </div>
-        </div>` :
+        </div>`:
         `<div class="row m-1">
             <div class="container">
                 <div class="col">
@@ -188,7 +186,7 @@ map.on('load', () => {
     });
     map.addControl(geocoder);
     // make ed active by default
-    registerLayerClick(divisions[0], false);
+    registerLayerClick(divisions[1], false);
 });
 
 // this is a custom event!
